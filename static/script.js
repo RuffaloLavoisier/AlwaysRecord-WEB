9009f89dc06f9957ece48a213aaf9cc284b39eb0
RuffaloLavoisier <jsh970307@daum.net>
2021-12-07 20:20:20 +0900

    add prank code

---
+++ static/script.js
@@ -0,0 +1,8 @@
+function clicked() {
+    alert("Actually, There's nothing.");
+    alert("Sorry, just do finger exercises.");
+    for (i = 0; i < 10000; i++) {
+        alert(10000 - i + " is left.");
+    }
+    document.write("<h1>It's done.</h1><br><br><h2>Thank you.</h2>");
+}
